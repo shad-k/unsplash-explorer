484084b14ab3467ba2a9bc5c48f964b1bf523b97
Shadab Khan <shadab.2005@gmail.com>
2021-03-19 11:41:46 +0530

    Add throttle util

---
+++ src/utils/throttle.ts
@@ -0,0 +1,18 @@
+const throttle = (func: any, limit: number = 1000) => {
+  let timeout: any;
+  let lastRun: number;
+  return (...args: any) => {
+    if(!lastRun) {
+      lastRun = Date.now();
+      func.call(this, ...args);
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        lastRun = Date.now();
+        func.call(this, ...args);
+      }, limit - (Date.now() - lastRun));
+    }
+  }
+}
+
+export default throttle;
