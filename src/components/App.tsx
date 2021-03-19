c18d29a5928e0c5ac82a40d387ebb4bd1f30dcaa
Shadab Khan <shadab.2005@gmail.com>
2021-03-19 10:54:50 +0530

    Add dependencies and routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import styled from 'styled-components';
+import Home from '../pages/Home';
+
+const Main = styled.main`
+  height: 100%;
+  width: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Main>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </Main>
   );
 }
 
